e30aa045f525915db53261a994f406f2d2300fd9
susheel123-sketch <susheelkumarnarsani@gmail.com>
2025-10-21 23:35:00 -0700

    task1

--- semester2/oops/lab7/task1.cpp
+++ semester2/oops/lab7/task1.cpp
@@ -5,3 +5,30 @@ Write a function void joinMatch(int age) that:
 • Throws a std::invalid_argument exception if the age is less than 16.
 • Displays "Access granted! You can join the match." otherwise.
 In main(), prompt the user for their age, call joinMatch, and handle any exceptions*/
+#include<iostream>
+#include<stdexcept>
+using namespace std;
+void join_match(int age)
+{
+    if(age<16)
+    {
+        throw runtime_error("You are not eligible for this game ");
+    }
+    cout<<"You are eligible for this game "<<endl;
+}
+int main()
+{
+    try
+    {
+        int age;
+        cout<<"Enter you age "<<endl;
+        cin>>age;
+        join_match(age);
+    }
+    catch(const runtime_error& e)
+    {
+        cout <<"Error:"<< e.what() << '\n';
+    }
+    
+    return 0;
+}
